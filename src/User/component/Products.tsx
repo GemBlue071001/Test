705be1e24c51aa9fd18dcdc362b577844a3c063e
Phuong Nguyen <85018054+nguyenlamthuyphuong25@users.noreply.github.com>
2022-05-30 09:37:08 +0700

    Update Products.tsx

--- src/User/component/Products.tsx
+++ src/User/component/Products.tsx
@@ -1,24 +1,43 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext, FC } from 'react'
 import { db, storage, fetchImages } from '../../firestore-config'
 import { ref, getDownloadURL } from 'firebase/storage'
 import { collection, getDocs } from 'firebase/firestore'
 
-export function Products() {
+export const Products: React.FC = () => {
+  const [items, setItems] = useState<any>([])
   const itemCollectionRef = collection(db, 'Items')
 
   useEffect(() => {
     const getItems = async () => {
       const data = await getDocs(itemCollectionRef)
-      var tmpItems: any = data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) 
+      var tmpItems: any = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }))
 
       for (var item of tmpItems) {
         console.log('GET IMAGE FOR ' + item.name)
         var imgUrl = await fetchImages(`Images/${item.imgName}`)
         item.imgUrl = imgUrl
       }
-
+      setItems(tmpItems)
       console.log(tmpItems)
     }
     getItems()
   }, [])
+
+  return (
+    <>
+      {items.length > 0 &&
+        items.map((item: any) => {
+          return (
+            <>
+              <h3>{item.name} </h3>
+              <h3>{item.price}$ </h3>
+              <img className="CartImg" src={item.imgUrl} alt="shop img" />
+            </>
+          )
+        })}
+    </>
+  )
 }
